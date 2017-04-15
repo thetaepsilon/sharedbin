c443e9c18cc6a58415210fc94d2bcc05a305ba5c
thetaepsilon <jordan@mearsvw.co.uk>
2017-04-15 10:41:42 +0100

    login marker shell: add /usr/local/bin to PATH

--- login-marker.sh
+++ login-marker.sh
@@ -3,6 +3,7 @@ id=$(id -un);
 dir="/run/loginmarker";
 if cat "${dir}/${id}" >&2; then
 	export SHELL=$(cat /etc/realshell.d/$id) || exit $?;
+	export PATH="$PATH:/usr/local/bin";
 	exec $SHELL || exit $?;
 else
 	echo "Permission denied";
