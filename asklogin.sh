ca3e6b90b39aeabf4200c8e1f194e24644c2d523
thetaepsilon <localhost>
2017-07-23 06:54:46 +0100

    login request script: shellcheck fixes (quoted variables, assign and export variable in separate steps)

--- asklogin.sh
+++ asklogin.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 id=$(id -un);
-reply=$(echo $id | socat STDIO,ignoreeof UNIX-CONNECT:/run/logincookie/ask);
+reply=$(echo "$id" | socat STDIO,ignoreeof UNIX-CONNECT:/run/logincookie/ask);
 #echo $reply;
 if test "$reply" == "$id"; then
-	export SHELL=$(cat /etc/realshell.d/$id) || exit $?;
-	exec $SHELL || exit $?;
+	SHELL=$(cat "/etc/realshell.d/$id") || exit $?;
+	export SHELL;
+	exec "$SHELL" || exit $?;
 else
 	echo "Permission denied";
 	exit 1;
