ecc565664072313c413f7bdb1a51da57c5bacd2c
thetaepsilon <thetaepsilon@users.noreply.github.com>
2017-09-25 09:26:08 +0100

    login-marker.sh: remove unnecessary path export as login shell /etc/profile processing does this

--- login-marker.sh
+++ login-marker.sh
@@ -4,7 +4,6 @@ dir="/run/loginmarker";
 if cat "${dir}/${id}" >&2; then
 	SHELL=$(cat "/etc/realshell.d/$id") || exit $?;
 	export SHELL;
-	export PATH="/usr/local/bin:$PATH";
 	exec "$SHELL" -l || exit $?;
 else
 	echo "Permission denied";
