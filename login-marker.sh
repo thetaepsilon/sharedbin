85e886c2033e78b2eaad5e19c7cc2896697fc3ab
thetaepsilon <thetaepsilon@users.noreply.github.com>
2017-09-25 09:23:40 +0100

    login-marker.sh: fix not invoking target shell as a login shell

--- login-marker.sh
+++ login-marker.sh
@@ -4,7 +4,7 @@ dir="/run/loginmarker";
 if cat "${dir}/${id}" >&2; then
 	export SHELL=$(cat /etc/realshell.d/$id) || exit $?;
 	export PATH="/usr/local/bin:$PATH";
-	exec $SHELL || exit $?;
+	exec "$SHELL" -l || exit $?;
 else
 	echo "Permission denied";
 	read;
