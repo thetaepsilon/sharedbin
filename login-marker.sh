4d5d2c03a16d84b6be554ecccc404e6b41c7c47e
thetaepsilon <thetaepsilon@users.noreply.github.com>
2017-09-25 09:24:44 +0100

    login-marker.sh: shellcheck fixes

--- login-marker.sh
+++ login-marker.sh
@@ -2,11 +2,12 @@
 id=$(id -un);
 dir="/run/loginmarker";
 if cat "${dir}/${id}" >&2; then
-	export SHELL=$(cat /etc/realshell.d/$id) || exit $?;
+	SHELL=$(cat "/etc/realshell.d/$id") || exit $?;
+	export SHELL;
 	export PATH="/usr/local/bin:$PATH";
 	exec "$SHELL" -l || exit $?;
 else
 	echo "Permission denied";
-	read;
+	read -r;
 	exit 1;
 fi
