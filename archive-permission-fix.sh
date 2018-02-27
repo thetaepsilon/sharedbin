a9695d707d4699fb8ae7bb15bc8ae4d5a51ed43b
thetaepsilon <thetaepsilon@users.noreply.github.com>
2018-02-27 11:31:21 +0000

    archive-permission-fix: add error checking, don't implicitly chown -R root

--- archive-permission-fix.sh
+++ archive-permission-fix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dir="$1";
-chown -R 0:0 "$dir";
-find "$dir" -type d -exec chmod 777 {} \;
-find "$dir" -type f -exec chmod 444 {} \;
+if test -z "$dir"; then echo "Usage: $0 path" >&2; exit 1; fi;
+find "$dir" -type d -exec chmod 777 {} \; &&
+find "$dir" -type f -exec chmod 444 {} \; || exit $?;
