666b1f6636df31ee41603907a563eb9efe551f71
thetaepsilon <thetaepsilon@users.noreply.github.com>
2018-03-04 10:26:44 +0000

    openscratchdev: use defaults of cryptsetup for plain mode cipher

--- openscratchdev.sh
+++ openscratchdev.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-exec cryptsetup open --type=plain --key-file=/dev/urandom --cipher=aes-xts-plain64 --key-size=512 -- "$1" "$2";
+exec cryptsetup open \
+	--type=plain \
+	--key-file=/dev/urandom \
+	-- "$1" "$2";
